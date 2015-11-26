4faa968899228d34e16e6664a6b9342cb9af6982
CruCk <microdharma@gmail.com>
2015-11-26 11:33:13 +0530

    Removed extra's

--- django_test/pepper/views.py
+++ django_test/pepper/views.py
@@ -5,8 +5,8 @@ from django.http import HttpResponse
 def search_form(request):
 	return render(request, 'search_form.html')
 def search(request):
-	if 'q' in request.GET:
-		message = 'You searched for: %r'%request.GET['q']
+	if 'a' in request.GET:
+		message = 'You searched for: %r'%request.GET['a']
 	else:
 		message = 'You submitted an empty form.'
 	return HttpResponse(message)
